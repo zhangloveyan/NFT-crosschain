45d4093c48e23a08b7d32945c6b3f0f222277adf
zhanglinan <zhanglinan>
2024-11-25 18:34:28 +0800

    unit test complete

--- test/cross-chain-nft.test.ts
+++ test/cross-chain-nft.test.ts
@@ -19,6 +19,7 @@ let nft: MyToken;
 let nftPoolLockAndRelease: NFTPoolLockAndRelease;
 let wnft: WrappedMyToken;
 let nftPoolBurnAndMint: NFTPoolBurnAndMint;
+let chainSelector: bigint;
 
 before(async function () {
     // const demo = await ethers.getSigners();
@@ -32,6 +33,8 @@ before(async function () {
     wnft = await ethers.getContract("WrappedMyToken", firstAccount);
     nftPoolBurnAndMint = await ethers.getContract("NFTPoolBurnAndMint", firstAccount);
 
+    const config = await ccip.configuration();
+    chainSelector = config.chainSelector_;
     // at 从合约地址 实例化 合约
     // 没有 at 是从 部署地址获取合约
     // const nftDep = await deployments.get("MyToken");
@@ -44,15 +47,61 @@ describe("原链功能测试", async function () {
         await nft.safeMint(secondAccount);
 
         const totalSupply = await nft.totalSupply();
-        console.log("总 mint 数量", totalSupply);
+        console.log("nft 总 mint 数量", totalSupply.toString());
 
         const owner_1: string = await nft.ownerOf(0);
         const owner_2: string = await nft.ownerOf(1);
         expect(owner_1).to.equal(firstAccount);
-        expect(owner_2).to.equal(secondAccount);
+        expect(owner_2).to.eq(secondAccount);
     })
 
     it("用户可以在原链 lock nft", async function () {
+        // 允许 合约转移 token
+        await nft.approve(nftPoolLockAndRelease.target, 0);
+        // 通过 ccip 给 nft lock 合约地址领水    
+        await ccip.requestLinkFromFaucet(
+            nftPoolLockAndRelease,
+            ethers.parseEther("10"));
+        // nft 锁定 并 发送消息 铸造新链
+        await nftPoolLockAndRelease.lockAndSendNFT(
+            0,
+            firstAccount,
+            chainSelector,
+            nftPoolBurnAndMint.target)
 
+        const owner = await nft.ownerOf(0);
+        expect(owner).equal(nftPoolLockAndRelease.target);
+    })
+
+    it("用户可以在目标链 mint wnft", async function () {
+        const totalSupply = await wnft.totalSupply();
+        console.log("wnft 总 mint 数量", totalSupply.toString());
+
+        const owner_1: string = await wnft.ownerOf(0);
+        expect(owner_1).to.equal(firstAccount);
+    })
+
+    it("测试 用户可以在目标链 burn wnft", async function () {
+        // 允许 合约转移 token
+        await wnft.approve(nftPoolBurnAndMint.target, 0);
+        await ccip.requestLinkFromFaucet(
+            nftPoolBurnAndMint,
+            ethers.parseEther("10"));
+        // nft 烧掉 并 发送消息 解锁原链
+        await nftPoolBurnAndMint.burnAndSendNFT(
+            0,
+            firstAccount,
+            chainSelector,
+            nftPoolLockAndRelease.target
+        )
+        const totalSupply = await wnft.totalSupply();
+        console.log("wnft 剩余数量", totalSupply.toString());
+        expect(0).eq(totalSupply);
+    })
+
+    it("用户可以在原链 release nft", async function () {
+
+        const owner = await nft.ownerOf(0);
+        expect(owner).equal(firstAccount);
     })
 })
