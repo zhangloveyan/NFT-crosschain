d715b49ee406f9e2d08dfd7a7da0075c7c7a480b
zhanglinan <zhanglinan>
2024-11-25 15:33:17 +0800

    test mint function

--- test/cross-chain-nft.test.ts
+++ test/cross-chain-nft.test.ts
@@ -1,8 +1,7 @@
-import { expect } from "chai";
-import { Contract } from "ethers";
-import { ethers, getNamedAccounts } from "hardhat";
-import {} from "@nomicfoundation/hardhat-toolbox";
-
+import { expect } from 'chai';
+import { deployments, ethers, getNamedAccounts } from 'hardhat';
+import { CCIPLocalSimulator } from './../typechain-types/@chainlink/local/src/ccip/CCIPLocalSimulator';
+import { MyToken, NFTPoolBurnAndMint, NFTPoolLockAndRelease, WrappedMyToken } from './../typechain-types/contracts';
 
 // 1.测试 用户可以成功 mint nft
 
@@ -15,27 +14,45 @@ import {} from "@nomicfoundation/hardhat-toolbox";
 // 测试前准备
 let firstAccount: string;
 let secondAccount: string;
-let ccip: Contract;
-let nft: Contract;
-let nftPoolLockAndRelease: Contract;
-let wnft: Contract;
-let nftPoolBurnAndMint: Contract;
+let ccip: CCIPLocalSimulator;
+let nft: MyToken;
+let nftPoolLockAndRelease: NFTPoolLockAndRelease;
+let wnft: WrappedMyToken;
+let nftPoolBurnAndMint: NFTPoolBurnAndMint;
+
 before(async function () {
-    const demo = await ethers.getSigners();
+    // const demo = await ethers.getSigners();
     firstAccount = (await getNamedAccounts()).firstAccount;
-    // const xx = await getNamedAccounts();
-    // firstAccount = xx.firstAccount;
     secondAccount = (await getNamedAccounts()).secondAccount;
-    const ccip = await ethers.getContractAt("CCIPLocalSimulator", firstAccount);
-    nft = await ethers.getContractAt("MyToken", firstAccount);
-    nftPoolLockAndRelease = await ethers.getContractAt("NFTPoolLockAndRelease", firstAccount);
-    wnft = await ethers.getContractAt("WrappedMyToken", firstAccount);
-    nftPoolBurnAndMint = await ethers.getContractAt("NFTPoolBurnAndMint", firstAccount);
+    await deployments.fixture(["all"]);
+
+    ccip = await ethers.getContract("CCIPLocalSimulator", firstAccount);
+    nft = await ethers.getContract("MyToken", firstAccount);
+    nftPoolLockAndRelease = await ethers.getContract("NFTPoolLockAndRelease", firstAccount);
+    wnft = await ethers.getContract("WrappedMyToken", firstAccount);
+    nftPoolBurnAndMint = await ethers.getContract("NFTPoolBurnAndMint", firstAccount);
+
+    // at 从合约地址 实例化 合约
+    // 没有 at 是从 部署地址获取合约
+    // const nftDep = await deployments.get("MyToken");
+    // nft = await ethers.getContractAt("MyToken", nftDep.address);
 })
 
 describe("原链功能测试", async function () {
     it("用户可以成功 mint nft", async function () {
-        await nft.saftMint(firstAccount);
-        expect(nft)
+        await nft.safeMint(firstAccount);
+        await nft.safeMint(secondAccount);
+
+        const totalSupply = await nft.totalSupply();
+        console.log("总 mint 数量", totalSupply);
+
+        const owner_1: string = await nft.ownerOf(0);
+        const owner_2: string = await nft.ownerOf(1);
+        expect(owner_1).to.equal(firstAccount);
+        expect(owner_2).to.equal(secondAccount);
+    })
+
+    it("用户可以在原链 lock nft", async function () {
+
     })
 })
