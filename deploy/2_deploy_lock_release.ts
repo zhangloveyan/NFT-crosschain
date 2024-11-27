228d972c40c5d6f62b142b633d1acde5769f8363
zhanglinan <zhanglinan>
2024-11-27 16:31:37 +0800

    deploy by network name

--- deploy/2_deploy_lock_release.ts
+++ deploy/2_deploy_lock_release.ts
@@ -1,5 +1,6 @@
-import { deployments, ethers, getNamedAccounts } from "hardhat";
+import { deployments, ethers, getNamedAccounts, network } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/dist/types";
+import { developementChains, networkConfig } from "../helper-hardhat-config";
 
 const deployNFTPoolLockAndRelease: DeployFunction = async () => {
     const { firstAccount } = await getNamedAccounts();
@@ -7,12 +8,22 @@ const deployNFTPoolLockAndRelease: DeployFunction = async () => {
 
     log("NFTPoolLockAndRelease 部署合约中。。。")
 
-    // 参数 address _router, address _link, address _nftAddr
-    const ccipDeployment = await deployments.get("CCIPLocalSimulator");
-    const ccip = await ethers.getContractAt("CCIPLocalSimulator", ccipDeployment.address);
-    const ccipConfig = await ccip.configuration();
-    const sourceRouter = ccipConfig.sourceRouter_;
-    const link = ccipConfig.linkToken_;
+    let sourceRouter: string;
+    let link: string;
+
+    if (developementChains.includes(network.name)) {
+        // 参数 address _router, address _link, address _nftAddr
+        const ccipDeployment = await deployments.get("CCIPLocalSimulator");
+        const ccip = await ethers.getContractAt("CCIPLocalSimulator", ccipDeployment.address);
+        const ccipConfig = await ccip.configuration();
+        sourceRouter = ccipConfig.sourceRouter_;
+        link = ccipConfig.linkToken_;
+    } else {
+        const chainId = network.config.chainId as number;
+        sourceRouter = networkConfig[chainId].router;
+        link = networkConfig[chainId].linkToken;
+
+    }
 
     const nftDeployment = await deployments.get("MyToken");
     // const nft = await ethers.getContractAt("MyToken", nftDeployment.address)
--- deploy/4_deploy_burn_mint.ts
+++ deploy/4_deploy_burn_mint.ts
@@ -1,17 +1,28 @@
-import { deployments, ethers, getNamedAccounts } from "hardhat";
+import { deployments, ethers, getNamedAccounts, network } from "hardhat";
 import { DeployFunction } from "hardhat-deploy/dist/types";
+import { developementChains, networkConfig } from "../helper-hardhat-config";
+
 const deployNFTPoolBurnAndMint: DeployFunction = async () => {
     const { firstAccount } = await getNamedAccounts();
     const { deploy, log } = deployments;
 
     log("NFTPoolBurnAndMint 部署合约中。。。")
 
-    // 参数 address _router, address _link, address _nftAddr
-    const ccipDeployment = await deployments.get("CCIPLocalSimulator");
-    const ccip = await ethers.getContractAt("CCIPLocalSimulator", ccipDeployment.address);
-    const ccipConfig = await ccip.configuration();
-    const destRouter = ccipConfig.destinationRouter_;
-    const link = ccipConfig.linkToken_;
+    let destRouter: string;
+    let link: string;
+
+    if (developementChains.includes(network.name)) {
+        // 参数 address _router, address _link, address _nftAddr
+        const ccipDeployment = await deployments.get("CCIPLocalSimulator");
+        const ccip = await ethers.getContractAt("CCIPLocalSimulator", ccipDeployment.address);
+        const ccipConfig = await ccip.configuration();
+        destRouter = ccipConfig.destinationRouter_;
+        link = ccipConfig.linkToken_;
+    } else {
+        const chainId = network.config.chainId as number;
+        destRouter = networkConfig[chainId].router;
+        link = networkConfig[chainId].linkToken;
+    }
 
     const wnftDeployment = await deployments.get("WrappedMyToken");
     const wnftAddr = wnftDeployment.address;
